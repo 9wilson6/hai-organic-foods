e1e849b45a89cce1eda369172421b85f5c6c5548
9wilson6 <gatheruwilson@gmail.com>
2023-10-15 17:14:15 +0300

    initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import "./globals.css";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: {
+    default: "Home | Hai Organic Foods",
+    template: "%s | Hai Organic Foods",
+  },
+  description:
+    "#1 Source of 100% Organic Food. We guarantee healthy eating from our products",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
